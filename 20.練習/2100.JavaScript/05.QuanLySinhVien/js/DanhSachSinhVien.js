617a06f93e4903ed2be1cfb917a07dbab62881cc
otsuki154 <nvthanh15490@gmail.com>
2022-11-09 12:03:05 +0900

    thêm file js

---
+++ 20.練習/2100.JavaScript/05.QuanLySinhVien/js/DanhSachSinhVien.js
@@ -0,0 +1,15 @@
+function DanhSachSinhVien() {
+  this.DSSV = [];
+
+  // Định nghĩa các method trong class
+  // Them sinh viên vào danh sách sinh viên
+  this.themSinhVien = function (sinhVien) {
+    this.DSSV.push(sinhvien);
+  };
+  // Xoá sinh viên trong danh sách sinh viên
+  this.xoaSinhVien = function (listSVXoa) {};
+
+  this.suaSinhVien = function (sinhVien) {};
+
+  this.timKiemSinhVien = function (sinhVien) {};
+}
---
+++ 20.練習/2100.JavaScript/05.QuanLySinhVien/js/SinhVien.js
@@ -0,0 +1,7 @@
+function SinhVien(maSV, tenSV, soChungMinhNhanDan, soDienThoai, email) {
+  this.maSV = maSV;
+  this.tenSV = tenSV;
+  this.soChungMinhNhanDan = soChungMinhNhanDan;
+  this.email = email;
+  this.soDienThoai = soDienThoai;
+}
